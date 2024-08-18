e79a2ae3681791a23f1631a9f349ae0b4e020365
mohamedeld <mohamed.azoz20010@gmail.com>
2024-08-18 20:25:07 +0300

    fetch data from graphql and display it

--- pages/portofilo/index.js
+++ pages/portofilo/index.js
@@ -1,5 +1,7 @@
+import axios from "axios"
 
-const Portofilo = () => {
+const Portofilo = ({portofilo}) => {
+  
   return (
     <div className="container">
       <section className="section-title">
@@ -8,45 +10,27 @@ const Portofilo = () => {
             <h1>Portfolios</h1>
           </div>
         </div>
+        <button className="btn btn-primary" onClick={()=>fetchData()}>Fetch Data</button>
       </section>
       <section className="pb-5">
         <div className="row">
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
+          {portofilo.data?.portfolios?.length> 0 ? portofilo.data?.portfolios?.map(item=>{
+            return (
+              <div className="col-md-4" key={item?._id}>
+              <div className="card subtle-shadow no-border">
+                <div className="card-body">
+                  <h5 className="card-title">{item?.title}</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                  <p className="card-text fs-2">{item?.description}</p>
+                </div>
+                <div className="card-footer no-border">
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
+            )})
+            : <p>Not Items found</p>}
+          
         </div>
       </section>
     </div>
@@ -54,3 +38,34 @@ const Portofilo = () => {
 }
 
 export default Portofilo
+
+export async function getServerSideProps(){
+  try{
+    const query = `
+    query Portfolios{
+      portfolios{
+        _id
+        title
+        company
+        companyWebsite
+        location
+        jobTitle
+        description
+      }
+    }
+    `;
+    const {data}= await axios.post('http://localhost:4000/graphql',{query});
+    return {
+      props:{
+        portofilo:data
+      }
+    }
+  }catch(error){
+    console.log(error);
+    return {
+      props: {
+        error: "Failed to fetch portfolios"
+      }
+    };
+  }
+}
--- server/index.js
+++ server/index.js
@@ -7,73 +7,75 @@ const {graphqlHTTP} = require("express-graphql");
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = app.getRequestHandler();
-
+const cors = require("cors")
 const data = {
   portfolio:[
     {
-      _id:"adsf21",
-      title:"Job in USB",
-      content:"welcome in programming",
-      jobTitle:"programmer",
-      daysOfExperience:12,
-      isCurrentEmployed:false
+      _id: "sad87da79",
+      title: 'Job in Netcentric',
+      company: 'Netcentric',
+      companyWebsite: 'www.google.com',
+      location: 'Spain, Barcelona',
+      jobTitle: 'Engineer',
+      description: 'Doing something, programing....',
+      startDate: '01/01/2014',
+      endDate: '01/01/2016'
     },
     {
-      _id:"ads545f21",
-      title:"Job in USB",
-      content:"welcome in programming",
-      jobTitle:"programmer",
-      daysOfExperience:12,
-      isCurrentEmployed:false
+      _id: "da789ad1",
+      title: 'Job in Siemens',
+      company: 'Siemens',
+      companyWebsite: 'www.google.com',
+      location: 'Slovakia, Kosice',
+      jobTitle: 'Software Engineer',
+      description: 'Responsoble for parsing framework for JSON medical data.',
+      startDate: '01/01/2011',
+      endDate: '01/01/2013'
     },
     {
-      _id:"ads333f21",
-      title:"Job in USB",
-      content:"welcome in programming",
-      jobTitle:"programmer",
-      daysOfExperience:12,
-      isCurrentEmployed:false
-    },
-    {
-      _id:"adsse1f21",
-      title:"Job in USB",
-      content:"welcome in programming",
-      jobTitle:"programmer",
-      daysOfExperience:12,
-      isCurrentEmployed:false
-    },
-    {
-      _id:"adssdff21",
-      title:"Job in USB",
-      content:"welcome in programming",
-      jobTitle:"programmer",
-      daysOfExperience:12,
-      isCurrentEmployed:false
-    },
+      _id: "sadcxv9",
+      title: 'Work in USA',
+      company: 'WhoKnows',
+      companyWebsite: 'www.google.com',
+      location: 'USA, Montana',
+      jobTitle: 'Housekeeping',
+      description: 'So much responsibility....Overloaaaaaad',
+      startDate: '01/01/2010',
+      endDate: '01/01/2011'
+    }
   ]
 }
 
 
 app.prepare().then(()=>{
   const server = express();
+  server.use(cors());
 
   const schema = buildSchema(`
+      type Portfolio{
+        _id:ID
+        title:String
+        company:String
+        companyWebsite:String
+        location:String
+        jobTitle:String
+        description:String
+        startDate:String
+        endDate:String
+      }
       type Query{
         hello:String
-        portfolio:{
-          _id:ID
-          title:String
-          content:String
-          jobTitle:String
-          daysOfExperience:Int
-          isCurrentEmployed:Boolean
-        }
+        portfolio:Portfolio
+        portfolios:[Portfolio]
       }
     `);
     const root ={
       hello:()=> "welcome",
       portfolio:()=>{
         return data.portfolio[0]
+      },
+      portfolios:()=>{
+        return data.portfolio
       }
     }
     server.use('/graphql',graphqlHTTP({
